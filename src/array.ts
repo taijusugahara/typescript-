caf76c39bd8535cfb8429862200f9d7ab8eb232a
taijusugahara <96ayutuki@gmail.com>
2022-04-03 11:20:09 +0900

    array

---
+++ src/array.ts
@@ -0,0 +1,19 @@
+
+export {};
+
+let numbers: number[] = [1,2,3];
+
+// 非推奨
+let numbers2: Array<number> = [1,2,3];
+
+console.log(numbers)
+
+let strings: string[] = ['type','java','php']
+console.log(strings)
+
+let nijigenhairetu: number[][] = [
+  [50,100],
+  [150,300]
+]
+
+let hairetu: (string | number | boolean)[] = [1,'japan',false]
