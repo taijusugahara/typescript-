f0cf1d9d02f36a87a23327203d4325a9b15e7de5
taijusugahara <96ayutuki@gmail.com>
2022-04-03 16:16:38 +0900

    enum

---
+++ src/enum.ts
@@ -0,0 +1,33 @@
+export {};
+
+enum Months {
+  January = 1,
+  February,
+  march,
+  April,
+  Month,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+}
+console.log(Months.January)
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = "#FFFFFF",
+  GREEN = "#000000"
+}
+
+let green = COLORS.GREEN
+
+enum COLORS{
+  YELLOW = "#FFFF00"
+}
+
+console.log(green)
+let yellow = COLORS.YELLOW
+console.log(yellow)
