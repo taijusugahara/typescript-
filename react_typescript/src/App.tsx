d0e107fc04ee1867ecd30d8fbbc6cd76d147db1a
taijusugahara <96ayutuki@gmail.com>
2022-04-10 13:04:43 +0900

    初type

---
+++ react_typescript/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface AppProps{
+  message:string;
+}
+const App = ({message}: AppProps) => {
+  return (
+    <div>
+      {message}
+    </div>
+  );
+}
+
+export default App;
